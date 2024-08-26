8ce10dc68412c922a01205dc59a9e68549397240
Bug <77031032+0zBug@users.noreply.github.com>
2024-08-26 00:18:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-// BUGTUNES.SCOT.WTF - Written by scot | <3 ty for making this site bug
-var colors = ["#8b99e1", "#8cc8a9", "#7ebae6", "#e0bb99", "#e0909e"];
-var colorIndex = 0;
 
-document.querySelectorAll("a").forEach(function(link){
-    var color = colors[colorIndex];
-    link.style.setProperty('--color', color);
-    colorIndex = (colorIndex + 1) % colors.length;
-});
+var colors = ["#8b99e1", "#8cc8a9", "#7ebae6", "#e0bb99", "#e0909e"];
+var pages = document.querySelectorAll('a.page');
+
+var numberOfElementsToColor = Math.floor(pages.length / 2);
+
+var coloredElements = new Set();
+
+while (coloredElements.size < numberOfElementsToColor) {
+    var randomIndex = Math.floor(Math.random() * pages.length);
+    
+    if (!coloredElements.has(randomIndex)) {
+        coloredElements.add(randomIndex);
+        var page = pages[randomIndex];
+        var randomColor = colors[Math.floor(Math.random() * colors.length)];
+        page.style.color = randomColor;
+    }
+}
