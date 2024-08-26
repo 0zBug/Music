78bfb856bf04d4a3b0d68bbfb42aba0c3135f76e
Bug <77031032+0zBug@users.noreply.github.com>
2024-08-26 00:31:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,5 @@
 var colors = ["#8b99e1", "#8cc8a9", "#7ebae6", "#e0bb99", "#e0909e"];
 
 document.querySelectorAll("a").forEach(function(link){
-    var color = colors[colorIndex];
     link.style.setProperty('--color', colors[Math.floor(Math.random() * colors.length)]);
-    colorIndex = (colorIndex + 1) % colors.length;
 });
